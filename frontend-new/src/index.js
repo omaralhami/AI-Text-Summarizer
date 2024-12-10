d1c33051386e2e6e841c516bd7bddf450a49224c
Omar Abdullah <imomarabdullah@gmail.com>
2024-12-10 09:32:22 +0300

    Update: Switch to HashRouter for GitHub Pages compatibility

--- frontend-new/src/index.js
+++ frontend-new/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -9,8 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 ); 
